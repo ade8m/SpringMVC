976eb9a093193636d7584e739f93e1b40585c698
ade8m <benammaradem83@gmail.com>
2024-03-11 22:39:35 +0100

    add new dependecy

---
+++ src/main/java/projet/spring/adem/AdemApplication.java
@@ -0,0 +1,13 @@
+package projet.spring.adem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/projet/spring/adem/AdemApplicationTests.java
@@ -0,0 +1,13 @@
+package projet.spring.adem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
